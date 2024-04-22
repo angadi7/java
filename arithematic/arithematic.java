e8a7fb0aa01a51bafcb49465fd92b4599379cbd8
angadi7 <167732764+angadi7@users.noreply.github.com>
2024-04-22 09:12:57 +0530

    Add files via upload

---
+++ arithematic/arithematic.java
@@ -0,0 +1,14 @@
+package arithematic;
+
+public class arithematic {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10;
+		int b=10;
+		System.out.println("Postincrement :"+ (a++));
+		System.out.println("Preincrement :"+ (++a));
+
+	}
+
+}
