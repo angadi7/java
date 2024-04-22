005c32c910c30fb09ec3c704e42264e28bc6d420
angadi7 <167732764+angadi7@users.noreply.github.com>
2024-04-22 09:20:32 +0530

    Add files via upload

---
+++ ListInterface/ListInterface.java
@@ -0,0 +1,28 @@
+package ListInterface;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListInterface {
+
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("1");
+        list.add("2");
+        list.add("3");
+        list.add("4");
+        list.add("5");
+        list.add("6");
+
+        System.out.println("Original list: " + list);
+
+        for (int i = list.size() - 1; i >= 0; i--) {
+            if (i % 2!= 0) { // Change condition to remove odd-indexed elements
+                String removedStr = list.remove(i);
+                System.out.println("Removed element: " + removedStr);
+            }
+        }
+
+        System.out.println("List after removing every second element: " + list);
+    }
+}
