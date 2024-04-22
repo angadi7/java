c010ea14964b7b8dc7f146c96533dce800093066
angadi7 <167732764+angadi7@users.noreply.github.com>
2024-04-22 09:38:15 +0530

    Update Fibonacci.java

--- Fibonacci/Fibonacci.java
+++ Fibonacci/Fibonacci.java
@@ -5,12 +5,12 @@ import java.util.Arrays;
 public class Fibonacci {
     public static long[] generateFibonacci(int n) {
         long[] fibonacciArray = new long[n];
-        // Check if n is less than 1
+        
         if (n < 1) {
-            return fibonacciArray; // Return an empty array if n is less than 1
+            return fibonacciArray; 
         }
 
-        // Generate the Fibonacci sequence
+        
         for (int i = 0; i < n; i++) {
             fibonacciArray[i] = fibonacci(i);
         }
@@ -26,13 +26,13 @@ public class Fibonacci {
     }
 
     public static void main(String[] args) {
-        int n = 10; // Number of elements in the Fibonacci sequence
+        int n = 10; 
         long[] fibonacciArray = generateFibonacci(n);
 
         System.out.println("First " + n + " elements of the Fibonacci sequence:");
         System.out.println(Arrays.toString(fibonacciArray));
 
-        int nthElement = 5; // Find the 5th element of the Fibonacci sequence
+        int nthElement = 5; 
         long nthFibonacci = fibonacci(nthElement);
         System.out.println("The " + nthElement + "th element of the Fibonacci sequence is: " + nthFibonacci);
     }
