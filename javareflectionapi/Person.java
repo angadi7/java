e34da0a215f3c5b2e26cbdff1d575f92df5ca1bf
angadi7 <167732764+angadi7@users.noreply.github.com>
2024-06-04 21:13:59 +0530

    Add files via upload

---
+++ javareflectionapi/Person.java
@@ -0,0 +1,37 @@
+package com.javareflectionapi;
+
+public class Person {
+    private String name;
+    private int age;
+ 
+    public Person() {
+    }
+ 
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+ 
+    public String getName() {
+        return name;
+    }
+ 
+    @SuppressWarnings("unused")
+	private void setName(String name) {
+        this.name = name;
+    }
+ 
+    public int getAge() {
+        return age;
+    }
+ 
+    @SuppressWarnings("unused")
+	private void setAge(int age) {
+        this.age = age;
+    }
+ 
+    @Override
+    public String toString() {
+        return "Person{name='" + name + "', age=" + age + '}';
+    }
+}
